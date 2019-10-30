3562d341b43d88e38efd14a7ed88ae2d95b33876
GENJIE YAO <gyao0001@student.monash.edu>
2019-10-30 15:25:15 +1100

    add wiki point

--- app1.js
+++ app1.js
@@ -6,6 +6,13 @@ app.get("/",function(req,res){
     res.send("Hello to my first Express App!!!");
 });
 
+app.get("/wiki/:keyword/fit2095/:id",function(req,res){
+    console.log(req.url);
+    console.log(req.params);
+
+    res.send("Hello to my first Express App!!!");
+});
+
 app.get("/about",function(req,res){
     res.send("This is the about page!!!")
 });
